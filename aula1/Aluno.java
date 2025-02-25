ef4a59ae95168defd1aea8c5a1c13ce8c4622941
CauaGNP <163431893+CauaGNP@users.noreply.github.com>
2025-02-25 21:45:48 +0000

    aula 1

---
+++ aula1/Aluno.java
@@ -0,0 +1,14 @@
+package aula1;
+
+public class Aluno {
+    public String nome;
+    public double altura;
+
+    public void estudar(String materia){
+        System.out.println(nome + " está estudando " + materia + "!!");
+    }
+
+    public void pedirNota(String prof){
+        System.out.println(nome + " está pedindo nota para o professor " + prof + "!! :C");
+    }
+}
---
+++ aula1/App.java
@@ -0,0 +1,22 @@
+package aula1;
+
+public class App {
+    public static void main(String[] args) {
+        Aluno aluno1 = new Aluno();
+        Aluno aluno2 = new Aluno();
+
+        aluno1.nome = "Cleber";
+        aluno1.altura = 1.70;
+        System.out.println("Nome: " + aluno1.nome);
+        System.out.println("Altura: " + aluno1.altura);
+        aluno1.estudar("POO");
+        aluno1.pedirNota("Iago");
+
+        aluno2.nome = "Pedrinho";
+        aluno2.altura = 1.40;
+        System.out.println("Nome: " + aluno2.nome);
+        System.out.println("Altura: " + aluno2.altura);
+        aluno2.estudar("Front-End");
+        aluno2.pedirNota("Marcio");
+    }
+}
