c47d5c568962344e397e99d92a48fbc0016cde59
CauaGNP <163431893+CauaGNP@users.noreply.github.com>
2025-03-11 22:46:11 +0000

    aula 3

---
+++ Aulas/aula3/Pessoa.java
@@ -0,0 +1,34 @@
+package Aulas.aula3;
+
+public class Pessoa {
+    private String nome;
+    private short idade;
+
+    public Pessoa(){
+
+    }
+
+    public Pessoa(String nome, short idade){
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public short getIdade(){
+        return idade;
+    }
+
+    public void setIdade(short idade){
+        if(idade > 17){
+            this.idade = idade;
+        }  
+    }
+
+}
